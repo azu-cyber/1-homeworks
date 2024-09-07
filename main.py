60c71a04ce78f631335917252513e439091f48cf
azu-cyber <avikalizhimomi@gmail.com>
2024-09-07 11:49:33 +0530

    length converter

--- main.py
+++ main.py
@@ -1,75 +1,40 @@
-import pygame 
-  
-  
-pygame.init() 
-  
-# Setting up the screen and timer 
-screen = pygame.display.set_mode((500, 500)) 
-timer = pygame.time.Clock() 
-  
-# set title 
-pygame.display.set_caption('Custom Events') 
-  
-# defining colours 
-WHITE = (255, 255, 255) 
-RED = (255, 0, 0) 
-GREEN = (0, 255, 0) 
-BLUE = (0, 0, 255) 
-  
-# Keep a track of active variable 
-bg_active_color = WHITE 
-screen.fill(WHITE) 
-  
-# custom user event to change color 
-CHANGE_COLOR = pygame.USEREVENT + 1
-  
-# custom user event to inflate defalte 
-# box 
-ON_BOX = pygame.USEREVENT + 2
-  
-# creating Rectangle 
-box = pygame.Rect((225, 225, 50, 50)) 
-grow = True
+from tkinter import *
+from tkinter import ttk
 
-pygame.time.set_timer(CHANGE_COLOR, 500) 
-  
-running = True
-while running: 
-    
-    for event in pygame.event.get(): 
-         if event.type == CHANGE_COLOR: 
-            if bg_active_color == GREEN: 
-                screen.fill(GREEN) 
-                bg_active_color = WHITE 
-            elif bg_active_color == WHITE: 
-                screen.fill(WHITE) 
-                bg_active_color = GREEN 
-  
-    if event.type == ON_BOX: 
-            
-            # to inflate and deflate box 
-            if grow: 
-                box.inflate_ip(3, 3) 
-                grow = box.width < 75
-            else: 
-                box.inflate_ip(-3, -3) 
-                grow = box.width < 50
-  
-    if event.type == pygame.QUIT: 
-            
-            # for quitting the program 
-            running = False
-  
-    # Posting event when the cursor is on top  
-    # of the box 
-    if box.collidepoint(pygame.mouse.get_pos()): 
-        pygame.event.post(pygame.event.Event(ON_BOX)) 
-  
-    # Drawing rectangle on the screen 
-    pygame.draw.rect(screen, RED, box) 
-    pygame.display.update() 
-      
-    # Setting Frames per Second 
-    timer.tick(30) 
-  
-pygame.quit() 
+def calculate(*args):
+    if choice.get() == 'meters':
+        value = float(inp.get())
+        result.set(int(3.281 * value * 10000.0 + 0.5) / 10000.0)
+        units.set('feet')
+    if choice.get() == 'feet':
+        value = float(inp.get())
+        result.set(int(0.3048 * value * 10000.0 + 0.5) / 10000.0)
+        units.set('meters')
+
+root = Tk()
+root.title('Length Converter')
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+mainframe = ttk.Frame(root, padding='25 25 25 25')
+mainframe.grid(sticky=(N, W, E, S))
+
+inp = StringVar()
+choice = StringVar()
+result = StringVar()
+units = StringVar()
+
+input_entry = ttk.Entry(mainframe, width=7, textvariable=inp)
+input_entry.grid(row=1, column=0)
+choices = ttk.Combobox(mainframe, width=10, textvariable=choice, values=('meters', 'feet'))
+choices.grid(row=1, column=1)
+
+ttk.Label(mainframe, text='is equal to').grid(row=2, column=0)
+ttk.Label(mainframe, textvariable=result).grid(row=2, column=1, sticky=E)
+ttk.Label(mainframe, textvariable=units).grid(row=2, column=2, sticky=W)
+
+ttk.Button(mainframe, text='Calculate', command=calculate).grid(row=3, column=1)
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+    root.mainloop()
